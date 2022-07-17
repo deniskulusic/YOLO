2b2b0fe70adb13f51b794058c735cf5b9074996c
deniskulusic <109351503+deniskulusic@users.noreply.github.com>
2022-07-17 18:29:54 +0200

    Delete YOLO directory

--- YOLO/js/faq.js
+++
@@ -1,6 +0,0 @@
-const acordation=document.getElementsByClassName('faq');
-for(i=0;i<acordation.length;i++){
-    acordation[i].addEventListener('click',function(){
-        this.classList.toggle('active');
-    })
-}
